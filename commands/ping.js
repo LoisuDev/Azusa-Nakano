aea1827758ee1e4352da9d48a3e14f87fa636157
Loïs Libessart <lois.libessart@gmail.com>
2020-12-06 16:40:13 +0100

    command/ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
     var ping = Date.now() - message.createdTimestamp + " ms";
-    message.channel.sendMessage("Your ping is `" + `${Date.now() - message.createdTimestamp}` + " ms`");
+    message.channel.send("Votre ping est de `" + `${Date.now() - message.createdTimestamp}` + " ms`");
 }
 module.exports.help = {
     name: "ping"
