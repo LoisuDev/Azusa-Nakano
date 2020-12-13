fa8f8ae174e9a17e6ff560fdea655643dd8aa873
Loïs Libessart <lois.libessart@gmail.com>
2020-12-13 16:40:49 +0100

    On branch master  Changes to be committed: 	modified:   commands/help.js 	modified:   commands/noel.js

--- commands/noel.js
+++ commands/noel.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
+    let noel_user = message.guild.member(message.mentions.members.first());
+    if (!noel_user) {
+     return message.channel.send("Cet utilisateur n'existe pas !")}
    let boticon = bot.user.displayAvatarURL();
    let noel_embed = new Discord.MessageEmbed()
     .setColor('e410d3')
     .setTitle('Joyeux Noël, Senpai ! ♥')
-    .setDescription("Le bot se revêtit pour Noël ! Amusez-vous bien les amis !")
+    .setDescription(`${noel_user.username}, ${message.author.username} te souhaite un joyeux Noël !`)
     .setThumbnail(boticon)
     .setImage('https://i.pinimg.com/originals/63/28/8e/63288ec10cedbb9146627535d2711a7a.gif')
 
