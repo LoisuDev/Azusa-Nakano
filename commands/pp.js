8b9beb9a8c63cc51a164bebaed53ee7d9dbac502
Loïs Libessart <lois.libessart@gmail.com>
2020-12-20 19:48:16 +0100

    On branch master  Changes to be committed: 	modified:   commands/pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -2,7 +2,7 @@ const Discord = require ('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     message.channel.send(`Voici l'avatar de : ${message.author.username}`)
-    return message.channel.send(message.author.avatar);
+    return message.channel.send(message.author.defaultAvatarURL);
 }
 
 module.exports.help = {
