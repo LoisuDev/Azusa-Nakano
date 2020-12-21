366b4dfc966f3166aa46d2227eb355a18a394a0a
Loïs Libessart <lois.libessart@gmail.com>
2020-12-21 18:24:20 +0100

    On branch master  Changes to be committed: 	modified:   commands/dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,6 +1,7 @@
 const Discord = require ('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission('MANAGE_SERVER')) return;
     let dm = args.join(" ");
     const members =  message.channel.guild.members.cache;
 
